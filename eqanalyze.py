b2d5d49a74f07a52d0fc466353f8f9d45f1e0537
Jamie Leibowitz <jamieleibowitz2016@u.northwestern.edu>
2019-05-05 19:56:23 -0500

    added file selection, fft for short files

--- eqanalyze.py
+++ eqanalyze.py
@@ -4,10 +4,37 @@
 # is to compare two files for the purpose of mixing
 # and mastering audio using reference tracks
 
-import wave
-import numpy
-import scipy
-import pydub
-import tkinter
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.fftpack as fftp
+import scipy.signal as sgnl
+from pydub import AudioSegment
+import tkinter as tk
 from tkinter import filedialog
+import sys
 
+# tkinter used to allow user to navigate their own file structure to find a sound file
+root = tk.Tk()
+root.withdraw()
+
+file1 = filedialog.askopenfilename(filetypes = (("mp3 files", "*.mp3"),("wav files", "*.wav")))
+
+# ensuring that the selected file type is opened correctly, then opening the file 
+# or exiting the program if no file was selected
+if not file1:
+    sys.exit()
+elif file1.lower().endswith('.mp3'):
+    song1 = AudioSegment.from_file(file1, format="mp3")
+elif file1.lower().endswith('.wav'):
+    song1 = AudioSegment.from_file(file1, format="wav")
+else:
+    print("Error: cannot open file of selected file type. Please select an mp3 or wav file")
+
+# the entire sound file is stored into an array
+f_s = song1.frame_rate
+song1Data = song1.get_array_of_samples()
+freqs = fftp.fftfreq(len(song1Data)) * f_s
+song1ft = fftp.fft(song1Data)
+
+plt.semilogx(freqs, np.log10(np.abs(song1ft))-6)
+plt.show()
